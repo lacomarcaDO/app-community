5a6a8f7f04adc28eb6c4d577d716771a2ae0526d
Starling Germosen <sgermosen@outlook.com>
2020-02-16 10:45:16 -0400

    Translate the App
    On this change, I make the services to get the current culture of the devices, and make translation of it, to use less burnet literals

---
+++ src/Community.Clients/Community.Clients/Community.Clients/Helpers/Languages.cs
@@ -0,0 +1,37 @@
+﻿using Community.Clients.Resources;
+using Community.Utils.Interfaces;
+using Xamarin.Forms;
+
+namespace Community.Clients.Helpers
+{
+    public static class Languages
+    {
+        static Languages()
+        {
+            var ci = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
+            Resource.Culture = ci;
+            DependencyService.Get<ILocalize>().SetLocale(ci);
+        }
+
+        public static string Accept => Resource.Accept;
+
+        public static string Error => Resource.Error;
+
+        public static string LaComarcaDo => Resource.LaComarcaDo;
+
+        public static string JoinToCommunity => Resource.JoinToCommunity;
+
+        public static string FollowUs => Resource.FollowUs;
+
+        public static string VisitOurWeb => Resource.VisitOurWeb;
+
+        public static string Main => Resource.Main;
+
+        public static string Blog => Resource.Blog;
+
+        public static string Events => Resource.Events;
+
+        public static string Info => Resource.Info;
+
+    }
+}
---
+++ src/Community.Clients/Community.Clients/Community.Clients/Interfaces/ILocalize.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+
+namespace Community.Clients.Interfaces
+{
+    public interface ILocalize
+    {
+        CultureInfo GetCurrentCultureInfo();
+
+        void SetLocale(CultureInfo ci);
+    }
+}
